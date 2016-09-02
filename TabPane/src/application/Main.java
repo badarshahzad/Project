0ded39f6cb3ba2202bcd60da57c03312f0c5ddc5
AbdulMalik206267 <am206267@gmail.com>
2016-09-02 14:43:12 +0500

    TabPane Code

---
+++ TabPane/src/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+	
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		startActivity(primaryStage);
+	}
+	
+	public void startActivity(Stage primaryStage){
+		
+		try {
+
+			Parent root = FXMLLoader.load(getClass().getResource("/application/MainWindowLayout.fxml"));
+			Scene scene = new Scene(root,1400,800);
+			
+			scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
